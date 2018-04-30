eb02bd4463bee03c6250239894ffd769a45eb35c
ralbert99 <33538199+ralbert99@users.noreply.github.com>
2018-04-30 00:11:36 -0500

    Week 14 | Updates, getting sass integrated

---
+++ littlemoments/wp-content/contact.php
@@ -0,0 +1,22 @@
+<!-- <?php echo basename(__FILE__); ?>-->
+
+<link href="https://fonts.googleapis.com/css?family=Lato|PT+Sans+Narrow" rel="stylesheet">
+
+
+<?php define( 'WP_USE_THEMES', false ); get_header(); ?>
+
+  <body <?php body_class(); ?>>
+
+    <header>
+      <h1>Little Moments</h1>
+      <h2>Contact</h2>
+    </header>
+
+    <footer>
+      <?php get_footer(); ?>
+    </footer>
+
+  </body>
+
+
+<!-- <?php echo "END OF " . basename(__FILE__); ?>-->
---
+++ littlemoments/wp-content/nav.php
@@ -0,0 +1,11 @@
+<!-- <?php echo basename(__FILE__); ?>-->
+
+<nav>
+  <ul>
+    <li><a href="/index.php">Home</a></li>
+    <li><a href="/about.php">About</a></li>
+    <li><a href="/contact.php">Contact</a></li>
+  </ul>
+</nav>
+
+<!-- <?php echo "END OF " . basename(__FILE__); ?>-->
