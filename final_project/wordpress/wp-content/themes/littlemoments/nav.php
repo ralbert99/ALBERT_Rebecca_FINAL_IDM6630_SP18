552c03a6d149b75e06ffdb3376b4773f8e66e18b
ralbert99 <33538199+ralbert99@users.noreply.github.com>
2018-05-03 20:58:50 -0500

    Continued Updates

--- final_project/wordpress/wp-content/themes/littlemoments/nav.php
+++ final_project/wordpress/wp-content/themes/littlemoments/nav.php
@@ -2,10 +2,10 @@
 
 <nav class="nav">
   <ul>
-    <li><a href="/index.php">Home</a></li>
-    <li><a href="/shop.php">Shop</a></li>
-    <li><a href="/about.php">About</a></li>
-    <li><a href="/contact.php">Contact</a></li>
+    <li><a href="<?php bloginfo('template_url'); ?> /index.php">Home</a></li>
+    <li><a href="<?php bloginfo('template_url'); ?> /shop.php">Shop</a></li>
+    <li><a href="<?php bloginfo('template_url'); ?> /about.php">About</a></li>
+    <li><a href="<?php bloginfo('template_url'); ?> /contact.php">Contact</a></li>
   </ul>
 </nav>
 
